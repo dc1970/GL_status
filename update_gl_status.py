43c370f1283f9bbeb96fbb56070d2507de993c4e
dc1970 <doron.caspi@gmail.com>
2020-08-29 14:15:16 -0400

    Add totals calculation

--- update_gl_status.py
+++ update_gl_status.py
@@ -1,9 +1,12 @@
 import csv
 
 if __name__ == '__main__':
-    csv_in_file = 'Arbel_GL-v3.csv'
-    csv_out_file = 'Arbel_GL-v4.csv'
+    csv_in_file = 'Arbel_GL-v4.csv'
+    csv_out_file = 'Arbel_GL-v5.csv'
     stat_file_name = 'DVTool_arbel_gld122g.stat'
+    total_tests = 0
+    total_pass = 0
+    total_fail = 0
 
     # Load statistics
     fast_stat = {}
@@ -21,16 +24,21 @@ if __name__ == '__main__':
     with open(csv_in_file, 'r') as file:
         csv_in_dict = csv.DictReader(file)
         for row in csv_in_dict:
+            total_tests += 1
             # Update Fast status
             if 'Pass' in row['Fast Status'] or 'Fail' in row['Fast Status']:
                 row['Fast Status'] = ''
             if row['Tests'] in fast_stat.keys():
                 row['Fast Status'] = fast_stat[row['Tests']]
+            if row['Fast Status'] == 'pass':
+                total_pass += 1
             # Update slow status
             if 'Pass' in row['Slow Status'] or 'Fail' in row['Slow Status']:
                 row['Slow Status'] = ''
             if row['Tests'] in slow_stat.keys():
                 row['Slow Status'] = slow_stat[row['Tests']]
+            if row['Slow Status'] == 'pass':
+                total_fail += 1
             result_dict.append(row)
 
     # Write updated result to new csv file
@@ -40,3 +48,7 @@ if __name__ == '__main__':
         writer.writeheader()
         for row in result_dict:
             writer.writerow(row)
+
+    print('Total Tests: ', total_tests)
+    print('Total passed in fast: ', total_pass)
+    print('Total passed in slow: ', total_fail)
